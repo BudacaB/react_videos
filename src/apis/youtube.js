06dc1e59c8d3037e900e55841b68ba543837e901
BudacaB <budaca.b@gmail.com>
2019-01-25 22:06:03 +0200

    youtube api, searching for videos, updating state with fetched data, passing state as props, rendering a list of videos, rendering video thumbnails, styling a list

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyApsVAswlHZcGNEur-_kDmEDgFBt0Su49A';
+
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
